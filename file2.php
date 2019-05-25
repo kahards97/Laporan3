6ba51cb19e8ee61f6f858228871de48a26ff957f
kahards97 <44362519+kahards97@users.noreply.github.com>
2019-05-25 23:14:37 +0800

    Update file2.php

--- file2.php
+++ file2.php
@@ -1,7 +1,9 @@
 <?
-	
+	echo "Laporan3 GitHub";	
+
 	echo "nama  : Kahar Muzakkar";
 	echo "NIM   : 60200116096";
 	echo "Kelas : Teknik Informatika C";
-
+	echo "fakultas : Sains dan Teknologi";
+	echo "Kampus :Universitas Islam Negeri Alauddin Makassar";
 ?>
